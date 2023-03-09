28d3c2aa55d2f8900ed56a8ef68d7ab0e955b17b
Pedram <pedadashii@gmail.com>
2023-03-09 15:19:06 +0330

    create book class

---
+++ src/book/Book.java
@@ -0,0 +1,21 @@
+package book;
+
+public abstract class Book {
+private String title;
+private double price;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void setPrice(double price);
+
+}
